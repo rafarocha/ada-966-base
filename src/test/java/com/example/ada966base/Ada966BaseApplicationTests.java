e724543bf9fc8f3dfd1cfc17ec779bf9fb22fb16
Rafael Rocha <rafael.rcavalcanti@dataprev.gov.br>
2023-02-17 15:52:15 -0300

    Initial commit

---
+++ src/test/java/com/example/ada966base/Ada966BaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ada966base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ada966BaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
